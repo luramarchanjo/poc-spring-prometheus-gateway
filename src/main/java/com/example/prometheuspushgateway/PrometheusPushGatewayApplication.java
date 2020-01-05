e7c959550e53aba6adf87f6f5b8081d6d47b01a2
Luram Archanjo <luram.archanjo@sensedia.com>
2020-01-05 08:15:31 -0300

    Adding .gitignore

---
+++ src/main/java/com/example/prometheuspushgateway/PrometheusPushGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.prometheuspushgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusPushGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrometheusPushGatewayApplication.class, args);
+	}
+
+}
